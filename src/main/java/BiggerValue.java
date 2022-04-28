2b799a1155c631b579dfade6fc74e0100fd55ab1
AnaLama <analama3744@gmail.com>
2022-04-28 16:10:02 -0400

    "commit 9.4"

--- src/main/java/BiggerValue.java
+++ src/main/java/BiggerValue.java
@@ -1,4 +1,9 @@
 public class BiggerValue {
 // Напишите алгоритм BiggerValue, который из двух параметров типа int возвращает бОльшее значение.
-
+public int getBiggerValue(int a, int b){
+    if (a > b){
+        return a;
+    }
+    else return b;
+}
 }
--- src/test/java/BiggerValueTest.java
+++ src/test/java/BiggerValueTest.java
@@ -1,5 +1,50 @@
+import org.junit.jupiter.api.*;
+
 public class BiggerValueTest {
 // Test Data:
 //3333, 9999
 //Expected Result = 9999
+    @Test
+        public void testBiggerValueHappyPath(){
+    int a = 3333;
+    int b = 9999;
+    int expectedResult = 9999;
+
+    BiggerValue bigVal = new BiggerValue();
+    int actualResult = bigVal.getBiggerValue(a , b);
+
+    }
+    @Test
+    public void testBiggerValueNegativeNumHappyPath(){
+        int a = -3333;
+        int b = -9999;
+        int expectedResult = -3333;
+
+        BiggerValue bigVal = new BiggerValue();
+        int actualResult = bigVal.getBiggerValue(a , b);
+
+    }
+    @Test
+    public void testBiggerValueBigNumHappyPath(){
+        int a = 3333;
+        int b = Integer.MAX_VALUE;
+        int expectedResult = Integer.MAX_VALUE;
+
+        BiggerValue bigVal = new BiggerValue();
+        int actualResult = bigVal.getBiggerValue(a , b);
+
+    }
+    @Test
+    public void testBiggerValueZeroHappyPath(){
+        int a = 0;
+        int b = 0;
+        int expectedResult = 0;
+
+        BiggerValue bigVal = new BiggerValue();
+        int actualResult = bigVal.getBiggerValue(a , b);
+
+    }
+
+
+
 }
