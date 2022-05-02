9d351ec1327483ca7ed88d643d06ced2585f1446
AnaLama <analama3744@gmail.com>
2022-05-02 16:53:39 -0400

    "commit 9.9"

--- src/main/java/Intersection.java
+++ src/main/java/Intersection.java
@@ -1,26 +1,30 @@
 public class Intersection {
-//Написать алгоритм Intersection, который принимает на вход 2 массива целых чисел и возвращает массив общих элементов.
-//     public int [] intersec (int []array1, int[]array2){
-//         if (array1.length > 0 && array2.length > 0) {
-//             int count = 0;
-//             int[]intersec = new int [count];
-//
-//         for (int i = 0; i < array1.length; i++){
-//             for (int j = 0; j < array2.length; j++){
-//                 if(array1[i] == array2[j]){
-//                     count++;
-//                 }
-//         }
-//              count = 0;
-//         }for (int i = 0; i < intersec.length; i++);
-//         if (array1[i] == array2[j]){
-//             intersec[i] = array1[i];
-//         }
-//
-//
-//         }
-//
-//
-//return  intersec;
-//}
-}
+    //Написать алгоритм Intersection, который принимает на вход 2 массива целых чисел и возвращает массив общих элементов.
+    public int[] intersec(int[] array1, int[] array2) {
+        if (array1.length > 0 && array2.length > 0) {
+            int count = 0;
+            for (int i = 0; i < array1.length; i++) {
+                for (int j = 0; j < array2.length; j++) {
+                    if (array1[i] == array2[j]) {
+                        count++;
+                    }
+                }
+            }
+            int[] intersec = new int[count];
+            count = 0;
+            for (int i = 0; i < array1.length; i++){
+                for (int j = 0; j < array2.length; j++) {
+                    if (array1[i] == array2[j]) {
+                        intersec[count] = array1[i];
+                        count++;
+                    }
+                }
+
+            }
+
+
+            return intersec;
+        }
+        return new int []{};
+    }
+        }
--- src/test/java/IntersectionTest.java
+++ src/test/java/IntersectionTest.java
@@ -1,2 +1,67 @@
+import org.junit.jupiter.api.*;
+
 public class IntersectionTest {
+//    Test Data:
+//    {1, 2, 4, 5, 89}, {8, 9, 4, 2} → {2, 4}
+//    {1, 2, 4, 5, 8, 9}, {8, 9, -4, -2} → {8, 9}
+//    {1, 2, 4, 5, 89}, {8, 9, 45} → {}
+
+    @Test
+    public void testIntersectionHappyPAth(){
+    int [] array1 = {1, 2, 4, 5, 89};
+    int []array2 = {8, 9, 4, 2};
+    int [] expectedResult = {2, 4};
+
+    Intersection intersec = new Intersection();
+    int [] actualResult = intersec.intersec(array1, array2);
+
+    Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testIntersectionNegNumHappyPAth(){
+        int [] array1 = {1, 2, 4, 5, 8, 9};
+        int []array2 = {8, 9, -4, -2};
+        int [] expectedResult = {8, 9};
+
+        Intersection intersec = new Intersection();
+        int [] actualResult = intersec.intersec(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void testNoIntersectionHappyPAth(){
+        int [] array1 = {1, 2, 4, 5, 89};
+        int []array2 = {8, 9, 45};
+        int [] expectedResult = {};
+
+        Intersection intersec = new Intersection();
+        int [] actualResult = intersec.intersec(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testIntersectionEmptyArrayHappyPAth(){
+        int [] array1 = {};
+        int []array2 = {8, 9, 45};
+        int [] expectedResult = {};
+
+        Intersection intersec = new Intersection();
+        int [] actualResult = intersec.intersec(array1, array2);
+
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
+
+//    @Test       // падает из-за длины массива
+//    public void testIntersectionZeroArrayHappyPAth(){
+//        int [] array1 = {0, 0, 0, 0, 0};
+//        int []array2 = {8, 0, 45};
+//        int [] expectedResult = {0};
+//
+//        Intersection intersec = new Intersection();
+//        int [] actualResult = intersec.intersec(array1, array2);
+//
+//        Assertions.assertArrayEquals(expectedResult, actualResult);
+//    }
 }
