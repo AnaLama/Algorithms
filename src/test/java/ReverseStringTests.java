a9144888bdeb8e880f77342b53a7155147d819fd
AnaLama <analama3744@gmail.com>
2022-05-09 17:12:16 -0400

    "commit 10.17.1"

--- src/test/java/ReverseStringTests.java
+++ src/test/java/ReverseStringTests.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.*;
 public class ReverseStringTests {
 
     @Test
-    public void String testReverseStringHappyPath(){
+    public void testReverseStringHappyPath(){
         String str = "Abracadabra";
         String expectedResult = "arbadacarbA";
         
         ReverseString revStr = new ReverseString();
-        StringactualResult = revStr.reverseString(str);
+        String actualResult = revStr.reverseString(str);
 
         Assertions.assertEquals(expectedResult, actualResult);
     }
