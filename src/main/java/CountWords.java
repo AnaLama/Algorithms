b52058d8708b38ee0f502947fa59ac327ee14421
AnaLama <analama3744@gmail.com>
2022-05-10 10:57:22 -0400

    "commit 10.17.2"

--- src/main/java/CountWords.java
+++ src/main/java/CountWords.java
@@ -1,20 +1,16 @@
 public class CountWords {
-// Напишите алгоритм CountWords, который принимает на вход текст и слово,  и считает все вариации этого слова в тексте
-public static int getNumberOfWords(String text, String word){
-    text = text.toLowerCase();
-    int count = 0;
-    String str = "";
+    // Напишите алгоритм CountWords, который принимает на вход текст и слово,  и считает все вариации этого слова в тексте
+    public static int getNumberOfWords(String text, String word) {
+        text = text.toLowerCase();
+        int count = 0;
+        String[] words = text.split(" ");
 
-//    for (int i = 0; i < text.length(); i++){
-//        if (text.charAt(i) == 'j') {
-//            str += text.substring(i, i+4);
-//            }}
-        for (int j = 0; j < text.length(); j ++){
-        if (text.substring(j, j+4) == "java"){
-            count++;}
-
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].replaceAll("[, .]", "");
+            if (words[i].equals(word)) {
+                count++;
+            }
+        }
+        return count;
     }
-
-    return count;
-}
 }
--- src/test/java/CountWordsTests.java
+++ src/test/java/CountWordsTests.java
@@ -3,8 +3,28 @@ import org.junit.jupiter.api.*;
 public class CountWordsTests {
     @Test
     public void testNumOfWordsHappyPath(){
-        String text = "ohf java fjtsa java";
+        String text = "As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support" +
+                " (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January 2019 " +
+                "for commercial java use, although it will otherwise still support Java 8 with public updates for personal " +
+                "use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are still " +
+                "receiving security and other upgrades";
         String word = "java";
+        int expectedResult = 5;
+
+        CountWords num = new CountWords();
+        int actualResult = num.getNumberOfWords(text, word);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testNumOfWords2HappyPath(){
+        String text = "As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support" +
+                " (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January 2019 " +
+                "for commercial java use, although it will otherwise still support Java 8 with public updates for personal" +
+                " use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are still receiving " +
+                "security and other upgrades";
+        String word = "version";
         int expectedResult = 2;
 
         CountWords num = new CountWords();
